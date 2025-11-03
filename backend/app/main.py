11b3afc80a27d8fd0e04507a195fd6cea960dab0
User <erati@gmael>
2025-11-03 20:50:15 +0200

    api

--- backend/app/main.py
+++ backend/app/main.py
@@ -1,3 +1,12 @@
 from fastapi import FastAPI
+from apps.users.routers import users_router
 
-app = FastAPI()
+def get_application() -> FastAPI:
+    app = FastAPI()
+
+    app.include_router(users_router, prefix='/users', tags=['Users', "Auth"])
+
+    return app
+
+
+app = get_application()
