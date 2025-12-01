81f3983dfc2aae40ded9472f69bfb79972b9e579
User <erati@gmael>
2025-12-01 19:18:42 +0200

    hcbv

--- frontend/app/router_user.py
+++ frontend/app/router_user.py
@@ -1,7 +1,6 @@
-from fastapi import Request, APIRouter
-
+from fastapi import Request, APIRouter, Form, status
+from fastapi.responses import RedirectResponse
 from fastapi.templating import Jinja2Templates
-
 templates = Jinja2Templates(directory="templates")
 
 router_user = APIRouter()
@@ -21,14 +20,16 @@ async def index(request: Request):
 
 @router_user.get("/register")
 @router_user.post("/register")
-async def user_register(request: Request):
-    print(request.method, 8888888888888)
+async def user_register(request: Request, email: str = Form(''), username: str = Form(''), password: str = Form('')):
     context = {
         "request": request,
-        "title": "Головна сторінка сайту",
+        "title": "Register",
         "user": {}
     }
+    if request.method == "GET":
+        response = templates.TemplateResponse('pages/register.html', context=context)
+        return response
 
-    response = templates.TemplateResponse('pages/register.html', context=context)
+    response = RedirectResponse(request.url_for('index'), status_code=status.HTTP_303_SEE_OTHER)
 
     return response
